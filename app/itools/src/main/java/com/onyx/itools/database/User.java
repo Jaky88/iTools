5c8003cc96a11f2ab3fae2910505f5f4c4ff5290
weijunjie <weijunjie@onyx-international.com>
2017-03-30 22:20:54 +0800

    add greendao.

---
+++ app/itools/src/main/java/com/onyx/itools/database/User.java
@@ -0,0 +1,43 @@
+package com.onyx.itools.database;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Transient;
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ * @Copyright: Copyright © 2017 Onyx International Inc. All rights reserved.
+ * @Project: iTools
+ * @Author: Jack
+ * @Date: 2017/3/30 0030,22:09
+ * @Version: V1.0
+ * @Description: TODO
+ */
+@Entity
+public class User {
+    @Id
+    private Long id;
+    private String name;
+    @Transient
+    private int tempUsageCount;
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    @Generated(hash = 873297011)
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    @Generated(hash = 586692638)
+    public User() {
+    }
+}
